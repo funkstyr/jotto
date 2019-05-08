bd3e259fa73802b871318331a90703ca61439b8e
Michael Funk <mfunk@brandsource.com>
2019-05-08 09:39:51 -0700

    msg

--- src/index.js
+++ src/index.js
@@ -18,5 +18,5 @@ ReactDOM.render(app, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// adding options; more pull
+// adding options; more pull; add github message
 serviceWorker.unregister();
